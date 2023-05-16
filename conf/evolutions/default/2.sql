980ef4cb9dc3b75108568f246dea022aa6543708
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2023-05-16 09:26:07 -0400

    fix: Fix sql typo

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -3,7 +3,7 @@
 create table stripe_user_payment_methods(
     user_id uuid not null constraint pk_stripe_user_payment_methods primary key,
     customer_id varchar not null,
-    payment_method_id varchat not null,
+    payment_method_id varchar not null,
     created_at timestamp not null default now(),
     updated_at timestamp not null default now()
 );
