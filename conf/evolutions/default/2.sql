605abcdcd999ce694dfa48f00f52e9cd821fef8f
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2023-07-26 15:49:17 -0400

    fix: Fix evols script

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,6 +1,10 @@
+# -- !Ups
+
 create table user_trial_status(
     user_id uuid not null constraint pk_user_trial_status primary key,
     has_been_used boolean not null,
     created_at timestamp not null default now(),
     updated_at timestamp not null default now()
 );
+
+# -- !Downs
